27e026fb30d7c75abc3e21acabd7c94159737cd8
RHensey <rbt.hensey@gmail.com>
2014-09-03 17:25:33 -0400

    Edited Person 1 file

--- src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -29,9 +29,8 @@ public class Person1 {
 	 * @param input the string to be modified
 	 * @return the modified string
 	 */
-	private String calc(String input) {
-	  //Person 1 put your implementation here
-	  return null;
+	private String calc(String input) { 
+	  return input.substring(2) + input.substring(0,2);
 	}
 	
 	/**
