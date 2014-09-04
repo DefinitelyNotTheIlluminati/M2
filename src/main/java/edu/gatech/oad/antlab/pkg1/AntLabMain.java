f279ad6d4ae35ed7f46ba3adfffbed31cc6c2809
Jeremy <jwetherford3@gatech.edu>
2014-09-04 19:52:01 -0400

    Added Robert to P1

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -57,8 +57,8 @@ import edu.gatech.oad.antlab.pkg2.*;
           + ant22.getMessage() + ant31.getMessage();
 		  //Person1 replace P1 with your name
 		  //and gburdell1 with your gt id
-		  Person1 p1 = new Person1("P1");
-		  toPrint += p1.toString("gburdell1");
+		  Person1 p1 = new Person1("Robert");
+		  toPrint += p1.toString("rhensey3");
 		  //Person2 replace P2 with your name
 		  //and gburdell with your gt id
 		  Person2 p2 = new Person2("Joseph");
@@ -73,7 +73,7 @@ import edu.gatech.oad.antlab.pkg2.*;
                   toPrint += p4.toString("gburdell4");
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("Rafael Blanes");
+                  Person5 p5 = new Person5("Rafael");
                   toPrint += p5.toString("rblanes3");
 		  
         System.out.println(toPrint);
